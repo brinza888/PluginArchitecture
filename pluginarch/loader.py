54bbd1ccbbd06c78b798fff8e27b11b47fa5f787
Brinza Bezrukoff <bezrukoff888@gmail.com>
2020-01-13 23:01:02 +0300

    Changed definition of stderr in Loader

--- pluginarch/loader.py
+++ pluginarch/loader.py
@@ -6,17 +6,18 @@ from .plugin import Plugin
 
 
 class Loader:
-    def __init__(self, path, plugin_cls):
+    def __init__(self, path, plugin_cls, stderr=sys.stderr):
         if not os.path.exists(path):
             raise NotADirectoryError("Cant find plugins directory")
-        if plugin_cls is not Plugin:
+        if plugin_cls not in Plugin.__subclasses__():
             raise TypeError("plugin_cls needs to implement plugin.Plugin")
+        self.stderr = stderr
         self.path = path
         self.plugin_cls = plugin_cls
         sys.path.append(self.path)
         self.plugins = []
 
-    def load(self, stderr=sys.stderr, *args, **kwargs):
+    def load(self, *args, **kwargs):
         files = os.listdir(self.path)
         for fname in files:
             module, ext = os.path.splitext(fname)
@@ -26,21 +27,21 @@ class Loader:
             try:
                 plugin = cls(*args, **kwargs)
             except Exception as ex:
-                stderr.write(f"{ex} caught during instantiate {cls}")
+                self.stderr.write(f"{ex} caught during instantiate {cls}")
                 continue
             try:
                 plugin.load()
             except Exception as ex:
-                stderr.write(f"{ex} caught during loading {plugin}")
+                self.stderr.write(f"{ex} caught during loading {plugin}")
                 continue
             self.plugins.append(plugin)
 
-    def unload(self, stderr=sys.stderr, *args, **kwargs):
+    def unload(self, *args, **kwargs):
         for plugin in self.plugins:
             try:
                 plugin.unload(*args, **kwargs)
             except Exception as ex:
-                stderr.write(f"{ex} caught during unloading {plugin}")
+                self.stderr.write(f"{ex} caught during unloading {plugin}")
 
     def get(self, name):
         for plugin in self.plugins:
